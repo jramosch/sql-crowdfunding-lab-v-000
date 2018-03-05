15411a40b627268810d906fe6d4f02f55bca56c1
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-05 16:02:41 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO users VALUES ("Danny",28);
+INSERT INTO users VALUES ("Alice",26);
+INSERT INTO users VALUES ("Boxer",18);
+INSERT INTO users VALUES ("Annabel",20);
+INSERT INTO users VALUES ("Jamilya",28);
+INSERT INTO users VALUES ("Carmel",24);
+INSERT INTO users VALUES ("Kenzo",27);
+INSERT INTO users VALUES ("August",21);
+INSERT INTO users VALUES ("Jerome",19);
+INSERT INTO users VALUES ("Eduardo",26);
+INSERT INTO users VALUES ("Alissa",28);
+INSERT INTO users VALUES ("Vianca",29);
+INSERT INTO users VALUES ("Naima",31);
+INSERT INTO users VALUES ("Maria",68);
+INSERT INTO users VALUES ("Nicole",33);
+INSERT INTO users VALUES ("Alexis",28);
+INSERT INTO users VALUES ("Amanda",22);
+INSERT INTO users VALUES ("Amanda",22);
+INSERT INTO users VALUES ("Amanda",22);
+INSERT INTO users VALUES ("Amanda",22);
+
+INSERT INTO projects VALUES ("Alpha", "A", 200, start_date, end_date)
